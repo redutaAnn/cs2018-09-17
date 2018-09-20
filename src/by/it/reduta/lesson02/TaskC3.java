117a76275e7e223ed904d1936e9a663f67002150
redutaAnn <redutaAnn@gmail.com>
2018-09-20 22:30:05 +0300

    lesson 2 reduta

---
+++ src/by/it/reduta/lesson02/TaskC3.java
@@ -0,0 +1,45 @@
+package by.it.reduta.lesson02;
+
+import java.util.Scanner;
+
+/*
+Ускорение свободного падения на Земле и Марсе таково:
+Марс   3.86
+Земля   9.81
+
+С клавиатуры вводится вес человека в килограммах.
+Рассчитайте вес человека на Марсе и выведите
+округлив его до сотых килограмма (2 знака)
+
+Создайте для этих целей метод getWeight(int weight)
+
+Требования:
+1. Метод getWeight(int weight) должен быть статическим.
+2. Метод getWeight должен возвращать значение типа double.
+3. Метод getWeight должен обязательно (!) округлять до сотых возвращаемое значение типа double.
+4. Метод getWeight не должен ничего выводить на экран.
+5. Метод getWeight должен правильно переводить вес тела в килограммах на Земле
+    в вес этого же тела на Марсе, и возвращать это значение.
+
+Пример:
+
+Ввод:
+75
+
+Вывод:
+29.51
+
+
+*/
+class TaskC3 {
+    public static void main(String[] args) {
+        System.out.println("");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println();
+        double weightPeopleEarth = keyboard.nextInt();
+        double weightPeopleMoon = weightPeopleEarth*0.17;
+        System.out.println("");
+        System.out.println("Ваш вес на Луне равен " + weightPeopleMoon + " кг.");
+    }
+
+}
