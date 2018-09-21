a8b8195a617195286b547836ab8362fba0e9123e
exqw33k <exqweek1@yandex.ru>
2018-09-21 12:05:58 +0300

    lesson 3

--- src/by/it/reduta/lesson02/TaskB3.java
+++ src/by/it/reduta/lesson02/TaskB3.java
@@ -15,7 +15,7 @@ class TaskB3 {
 //        System.out.println("C Новым");
 //        System.out.println("Годом");
 //        System.out.print("C Новым");
-//        System.out.println("Годом");
+//        System.out.print("Годом");
 //        System.out.println(s);
 //        System.out.print("C Новым!");
 //        System.out.print(" ");
